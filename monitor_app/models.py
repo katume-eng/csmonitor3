7fb05382bf4a4aa7bd492a637a20a50bbddc8257
shogo <0814.sho.mura@gmail.com>
2025-01-12 06:19:50 +0900

    make models

--- monitor_app/models.py
+++ monitor_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-class Monitor(models.Model):
+class Location(models.Model):
+    program_name = models.CharField(max_length=255)
+    room_name = models.CharField(max_length=255)
+    floor = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.program_name} - {self.room_name} - {self.floor}"
+
+class Collected(models.Model):
     location = models.ForeignKey('Location', on_delete=models.CASCADE)
     congestion_level = models.IntegerField()
     published_at = models.DateTimeField()
@@ -8,9 +18,7 @@ class Monitor(models.Model):
     def __str__(self):
         return f"{self.location.name} - {self.congestion_level.level} - {self.published_at}"
 
-class Location(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
+    
+class CongestionLevel(models.Model):
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    level = models.IntegerField(default=0)
--- monitor_app/views.py
+++ monitor_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Location, Collected, CongestionLevel
 
 def initial(request):
     return HttpResponse("Hello, World!")
-
