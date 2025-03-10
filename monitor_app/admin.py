f5f560924fb2b3448f5a293e42aa025bcbfa22ca
shogo <0814.sho.mura@gmail.com>
2025-03-10 20:56:13 +0900

    add Congestion level into admin

--- monitor_app/admin.py
+++ monitor_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Collected, Location
+from .models import Collected, Location, CongestionLevel
 
 admin.site.register(Collected)
 admin.site.register(Location)
+admin.site.register(CongestionLevel)
