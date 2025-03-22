0895786bb3ad31275e058e13fa770fe94534a8bf
shogo <0814.sho.mura@gmail.com>
2025-03-22 19:25:41 +0900

    add integer arguments to url of display to make it able to show each floor's programs.

--- monitor_app/urls.py
+++ monitor_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("initial/", views.initial, name="initial"),
     path("aggregates_data/", views.aggregates_data, name="aggregates_data"),
     path("display/", views.display, name="display"),
+    path("displayf/<int:floor>/", views.displayf, name="display"),
     path("make_random_data/", views.make_random_data, name="make_random_data"),
 ]
