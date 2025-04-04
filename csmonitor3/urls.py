6af96ad2397aa81a6675f68fc4e4317cd0a284fa
shogo <0814.sho.mura@gmail.com>
2025-04-04 18:44:26 +0900

    routing setting

--- csmonitor3/urls.py
+++ csmonitor3/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("monitor/" , include("monitor_app.urls")),
+    path("" , include("monitor_app.urls")),
 ]
