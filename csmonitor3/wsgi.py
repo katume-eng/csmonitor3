d0009d7d263f7eda897100c3aa2cae8a4032d2c8
shogo <0814.sho.mura@gmail.com>
2025-08-04 22:57:12 +0900

    staticfilesの設定と、wsgi.pyの設定

--- csmonitor3/wsgi.py
+++ csmonitor3/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for csmonitor3 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csmonitor3.settings')
 
 application = get_wsgi_application()
+
+# WhiteNoise を設定（gzip圧縮やキャッシュ対応も有効に）
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'), prefix='static/')
