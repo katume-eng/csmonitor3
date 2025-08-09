379973d9820733c4d150b29e416ebef6a76dec81
shogo <0814.sho.mura@gmail.com>
2025-08-09 22:33:49 +0900

    display_json_apiをついかして display_jsonを消去

--- monitor_app/urls.py
+++ monitor_app/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path("initial/", views.initial, name="initial"),
     path("aggregates_data/", views.aggregates_data, name="aggregates_data"),
     path("display/<int:floor_given>/", views.display, name="display"),
-    path("display_json/",views.display_json, name="display_json"),
+    # path("display_json/",views.display_json, name="display_json"),
     path("display_json_api/",views.display_json_api, name="display_json_api"),
     # path("displayf/<int:floor>/", views.displayf, name="display"),
-    path("make_random_data/", views.make_random_data, name="make_random_data"),
+    # path("make_random_data/", views.make_random_data, name="make_random_data"),
+    # path("test/", views.test, name="test"),
 ]
