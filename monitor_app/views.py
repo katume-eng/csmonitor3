c2b6ec823e98edbb169343226c5367734415327e
shogo <0814.sho.mura@gmail.com>
2025-09-04 22:59:40 +0900

    floor.jsonにfloor_local_idを入れる

--- monitor_app/views.py
+++ monitor_app/views.py
@@ -94,6 +94,7 @@ def display_json_api(request):
         for it in data:
             key = str(it['floor'])
             floors.setdefault(key, []).append({
+                "floor_local_id": it["floor_local_id"],
                 "program_name": it["program_name"],
                 "room_name": it["room_name"],
                 "level": it["level"],
