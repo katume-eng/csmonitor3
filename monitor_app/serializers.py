57184b625d07431b7d546dc609cb22cf18989727
shogo <0814.sho.mura@gmail.com>
2025-09-04 21:45:41 +0900

    シリアライザーにコメント欄はいらない

--- monitor_app/serializers.py
+++ monitor_app/serializers.py
@@ -6,12 +6,11 @@ class CongestionLevelItemSerializer(serializers.ModelSerializer):
     # Locationの項目を直で出したいので source= で外部キー先を参照
     program_name = serializers.CharField(source='location.program_name', read_only=True)
     room_name    = serializers.CharField(source='location.room_name', read_only=True)
-    comment      = serializers.CharField(source='location.comment', read_only=True)
     floor        = serializers.IntegerField(source='location.floor', read_only=True)
 
     class Meta:
         model  = CongestionLevel
-        fields = ['program_name', 'room_name', 'level', 'reliability', 'comment', 'floor']
+        fields = ['program_name', 'room_name', 'level', 'reliability', 'floor']
 
 class CongestionLevelCreateSerializer(serializers.ModelSerializer):
     class Meta:
