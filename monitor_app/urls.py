faeae1f9921cc9ae26f1d10e84fd2d1f4d425df6
shogo <0814.sho.mura@gmail.com>
2025-09-05 13:53:38 +0900

    公開させとく

--- monitor_app/urls.py
+++ monitor_app/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("vote/", views.con_form_view, name="vote"),
-    # path("aggregates_data/", views.aggregates_data, name="aggregates_data"),
-    # path("display/<int:floor_given>/", views.display, name="display"),
-    # path("display_json_api/",views.display_json_api, name="display_json_api"),
-    # path("food_cs_api/", views.food_cs_api, name="zero_api"),
+    path("vote/", views.con_form_view, name="vote"),
+    path("aggregates_data/", views.aggregates_data, name="aggregates_data"),
+    path("display/<int:floor_given>/", views.display, name="display"),
+    path("display_json_api/",views.display_json_api, name="display_json_api"),
+    path("food_cs_api/", views.food_cs_api, name="zero_api"),
     # path("make_random_data/", views.make_random_data, name="make_random_data"),
 ]
